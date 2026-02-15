ec0cd693b84c6d800405e36a5ac21d3746598c67
Shirasagi0012 <shirasagi.0012@gmail.com>
2026-02-15 22:29:49 +0800

    refactor: deprecated old implementation, prepare for new markup extension based impl

---
+++ MaterialColorUtilities.Avalonia/MaterialColor.cs
@@ -0,0 +1,20 @@
+using Avalonia;
+
+namespace MaterialColorUtilities.Avalonia;
+
+public class MaterialColor : AvaloniaObject
+{
+    public static readonly StyledProperty<MaterialColorScheme?> SchemeProperty =
+        AvaloniaProperty.RegisterAttached<MaterialColor, AvaloniaObject, MaterialColorScheme?>(
+            "Scheme", inherits: true);
+
+    public static MaterialColorScheme? GetScheme(AvaloniaObject element)
+    {
+        return element.GetValue(SchemeProperty);
+    }
+
+    public static void SetScheme(AvaloniaObject element, MaterialColorScheme? value)
+    {
+        element.SetValue(SchemeProperty, value);
+    }
+}
---
+++ MaterialColorUtilities.Avalonia/MaterialColorScheme.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Media;
+using Avalonia.Styling;
+using MaterialColorUtilities.DynamicColors;
+
+namespace MaterialColorUtilities.Avalonia;
+
+public class MaterialColorScheme : AvaloniaObject
+{
+    public static readonly DirectProperty<MaterialColorScheme, ISchemeProvider?> SchemeProperty =
+        AvaloniaProperty.RegisterDirect<MaterialColorScheme, ISchemeProvider?>(
+            nameof(Scheme),
+            palette => palette.Scheme,
+            (palette, provider) => palette.Scheme = provider
+        );
+
+    public ISchemeProvider? Scheme
+    {
+        get;
+        set => SetAndRaise(SchemeProperty, ref field, value);
+    }
+
+    private DynamicScheme? _scheme;
+
+    public MaterialColorScheme()
+    {
+        SchemeProperty.Changed.AddClassHandler<MaterialColorScheme, ISchemeProvider?>((target, args) =>
+        {
+            target._scheme = args.NewValue.GetValueOrDefault()?.CreateScheme(ThemeVariant.Default);
+        });
+    }
+
+    public Color? GetColor(string color)
+        => color switch
+        {
+            "primary" => _scheme?.Primary.ToAvaloniaColor(),
+            "onPrimary" => _scheme?.OnPrimary.ToAvaloniaColor(),
+            "primaryContainer" => _scheme?.PrimaryContainer.ToAvaloniaColor(),
+            "onPrimaryContainer" => _scheme?.OnPrimaryContainer.ToAvaloniaColor(),
+            _ => Colors.Black
+        };
+    
+    public MaterialColorScheme ProvideValue(IServiceProvider serviceProvider) => this;
+}
